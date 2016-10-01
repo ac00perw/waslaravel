8a4d82e1738d829e43c876ed1db4ae9f794824b6
Adam <a@acdubs.com>
2016-10-01 09:00:48 -0400

    fancify 404

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -3,7 +3,7 @@
 @section('content')
         <div class="container">
             <div class="content">
-                <h1>Shit. 404 error.</h1>
+                <h1>Nuts. 404 error.</h1>
             </div>
         </div>
 @stop
